44b60aee53a3f79f0e2b525354127f54b810bf7e
shivank <shivank.rajput2005@gmail.com>
2022-11-12 23:09:18 +0300

    solved part2

--- 2020/2020_d9.py
+++ 2020/2020_d9.py
@@ -15,7 +15,21 @@ def solve(nums, prelen=25):
             return nums[i]
 
 #part 1
-#print(solve(inn))
+print(solve(inn))
 
 #part 2
 invalid = solve(inn)
+print(inn)
+def setgen(nums):
+    for i in range(len(nums)-1):
+        for j in range(i+2, len(nums)):
+            numset = nums[i:j]
+            yield numset
+
+sets = setgen(inn)
+for i in sets:
+    # print(i)
+    if sum(i)==invalid:
+        ans = min(i) + max(i)
+        print(ans)
+        break
