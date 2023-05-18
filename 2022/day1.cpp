026d1ac992f4d6989db9170f19044e0e20006491
shivank <shivank.rajput2005@gmail.com>
2023-05-19 00:55:01 +0300

    day 1 in cpp

---
+++ 2022/day1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    ifstream input ("input.txt", ifstream::in);
+    string val;
+    u_int64_t sum = 0;
+    vector<u_int64_t> sums;
+    while(getline(input, val))
+    {
+        
+        if(!val.empty())
+        {
+            sum+=stoul(val);
+        }
+        else
+        {
+            sums.push_back(sum);
+            sum = 0;
+        }
+
+    }
+    sort(sums.begin(), sums.end(), greater<u_int64_t>());
+
+    //part 1
+    cout << "Part 1: " << sums[0] << endl;
+
+    //part 2
+    cout << "Part 2: " << sums[0]+sums[1]+sums[2] << endl;
+
+}
